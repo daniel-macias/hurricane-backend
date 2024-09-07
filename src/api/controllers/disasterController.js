727fc96e8976be881d1459abbe3e8c95b6c39687
daniel-macias <josedanielmr@gmail.com>
2024-09-06 20:20:29 -0600

    added last earthquakes in 24 hours

--- src/api/controllers/disasterController.js
+++ src/api/controllers/disasterController.js
@@ -1,5 +1,25 @@
-exports.getEarthquakes = (req, res) => {
-    res.send('Earthquake data will be here.');
+const axios = require('axios');
+
+exports.getEarthquakes = async (req, res) => {
+    try {
+        // Get current date and the date 24 hours ago
+        const endDate = new Date();
+        const startDate = new Date(new Date().setDate(endDate.getDate() - 1));
+        const formatDateString = (date) => date.toISOString().split('T')[0];
+
+        const response = await axios.get('https://earthquake.usgs.gov/fdsnws/event/1/query', {
+            params: {
+                format: 'geojson',
+                starttime: formatDateString(startDate),
+                endtime: formatDateString(endDate)
+            }
+        });
+
+        res.json(response.data);
+    } catch (error) {
+        console.error('Failed to fetch earthquake data:', error);
+        res.status(500).send('Failed to fetch earthquake data');
+    }
 };
 
 exports.getHurricanes = (req, res) => {
